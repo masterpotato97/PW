db77168e86beec72721762c5a457820735ae9fa3
Daniel Herrera <dannyherrea97@gmail.com>
2023-06-28 12:48:25 -0500

    1

--- prework.py
+++ prework.py
@@ -2,8 +2,9 @@
 # Question 1
 # Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
 def hello_name(user_name):
-        message = "Hello world!"
+        message = "Hello " + user_name
         print(message)
+hello_name("Daniel")
                 
 # Question 2
 # Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
